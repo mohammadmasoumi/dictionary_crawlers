ba9f8de2e3370d94c42be7b0ba3fcad691d1260f
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-17 01:57:24 +0330

    add longman

--- dictionary_crawlers/dictionary_crawlers/services/base.py
+++ dictionary_crawlers/dictionary_crawlers/services/base.py
@@ -13,6 +13,15 @@ class BaseService:
         return word.strip() if strip else word
 
     @staticmethod
+    def _first(iterables: list):
+        """
+
+        :param iterables:
+        :return:
+        """
+        return iterables[0] if iterables else None
+
+    @staticmethod
     def _join_url(base_url, href: list):
         """
 
--- dictionary_crawlers/dictionary_crawlers/services/longman.py
+++ dictionary_crawlers/dictionary_crawlers/services/longman.py
@@ -1,5 +1,5 @@
 import logging
-
+import ujson
 from lxml import etree
 
 from .base import BaseService
@@ -32,7 +32,7 @@ class LongManDefinitionService(BaseService):
         header = {}
         for key, xpath in self._HEADER_XPATH_MAPPING.items():
             value = tree.xpath(xpath)
-            header.update({key: value[-1]}) if value else None
+            header.update({key: self._first(value)}) if value else None
 
         return header
 
@@ -46,7 +46,7 @@ class LongManDefinitionService(BaseService):
         for example in tree.xpath("span[@class='EXAMPLE']"):
             items.append({
                 'example': self._join(example.xpath("text()")),
-                'audio': example.xpath("span[1]//@data-src-mp3")
+                'audio': self._first(example.xpath("span[1]//@data-src-mp3"))
             })
         return items
 
@@ -60,7 +60,8 @@ class LongManDefinitionService(BaseService):
         for grammar_example in tree.xpath("span[@class='GramExa']"):
             items.append({
                 'example': self._join(grammar_example.xpath("span[1]//text()")),
-                'audio': grammar_example.xpath("span[2]//@data-src-mp3")
+                'audio': self._first(grammar_example.xpath("span[2]//@data-src-mp3")),
+                'text': self._join(grammar_example.xpath("span[2]/text()"))
             })
         return items
 
@@ -74,7 +75,7 @@ class LongManDefinitionService(BaseService):
         for collocation_example in tree.xpath("span[@class='ColloExa']"):
             items.append({
                 'example': self._join(collocation_example.xpath("span[1]//text()")),
-                'audio': collocation_example.xpath("span[2]//@data-src-mp3")
+                'audio': self._first(collocation_example.xpath("span[2]//@data-src-mp3"))
             })
         return items
 
@@ -85,10 +86,10 @@ class LongManDefinitionService(BaseService):
         :return:
         """
         items = []
-        for cross_ref in tree.xpath("span[@class='Crossref']//a"):
+        for cross_ref in tree.xpath("span[@class='Crossref']/a"):
             items.append({
                 'example': self._join(cross_ref.xpath("@title")),
-                'audio': self._join_url(LONGMAN_SITE_URL, cross_ref.xpath("@href"))
+                'link': self._join_url(LONGMAN_SITE_URL, cross_ref.xpath("@href"))
             })
         return items
 
@@ -102,6 +103,9 @@ class LongManDefinitionService(BaseService):
             'sense_num': self._join(tree.xpath("span[1]//text()")),
             'sign_post': self._join(tree.xpath("span[@class='SIGNPOST']//text()")),
             'gram': self._join(tree.xpath("span[@class='GRAM']//text()")),
+            'def': self._join(tree.xpath("span[@class='DEF']//text()")),
+            'field': self._join(tree.xpath("span[@class='FIELD']//text()")),
+            'active': self._join(tree.xpath("span[@class='ACTIV']//text()")),
         }
 
     def _process_sub_sense(self, tree):
@@ -114,14 +118,13 @@ class LongManDefinitionService(BaseService):
             'sub_sense_num': self._join(tree.xpath("span[1]//text()")),
             'active': self._join(tree.xpath("span[@class='ACTIV']//text()")),
             'geo': self._join(tree.xpath("span[@class='GEO']//text()")),
-            'syn': self._join(tree.xpath("span[@class='SYN']//text()")),
+            'syn': self._join(tree.xpath("span[@class='SYN']/text()")),
             'main_definition': self._join(tree.xpath("span[@class='DEF']//text()")),
         }
 
     def process(self):
         definitions = {}
 
-        logger.debug("######################################################")
         for item in self._items:
             iterable_item = item.strip() if isinstance(item, str) else None
 
@@ -129,37 +132,59 @@ class LongManDefinitionService(BaseService):
                 html = etree.HTML(iterable_item)
 
                 headers = self._process_header(html)
-                refs = self._process_refs(tree=html)
-                examples = self._process_example(tree=html)
-                collocation_examples = self._process_collocation_example(tree=html)
-                grammar_examples = self._process_grammar_example(tree=html)
 
-                logger.debug(f"headers: {headers}")
-                logger.debug(f"refs: {refs}")
-                logger.debug(f"examples: {examples}")
-                logger.debug(f"collocation_examples: {collocation_examples}")
-                logger.debug(f"grammar_examples: {grammar_examples}")
+                if headers:
+                    refs = self._process_refs(tree=html)
+                    examples = self._process_example(tree=html)
+                    collocation_examples = self._process_collocation_example(tree=html)
+                    grammar_examples = self._process_grammar_example(tree=html)
 
-                # definition
-                for sense in html.xpath("//span[@class='Sense']"):
-                    logger.debug("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-                    process_sense = self._process_sense(tree=sense)
+                    headers.update({'refs': refs}) if refs else None
+                    headers.update({'examples': examples}) if examples else None
+                    headers.update({'collocation_examples': collocation_examples}) if collocation_examples else None
+                    headers.update({'grammar_examples': grammar_examples}) if grammar_examples else None
 
-                    logger.debug(f"process_sense: {process_sense}")
+                    # definition
+                    senses = []
+                    for sense in html.xpath("//span[@class='Sense']"):
+                        process_sense = self._process_sense(tree=sense)
 
-                    for sub_sense in sense.xpath("span[@class='Subsense']"):
-                        process_sub_sense = self._process_sub_sense(tree=sub_sense)
-                        sub_refs = self._process_refs(tree=html)
-                        sub_examples = self._process_example(tree=html)
-                        sub_collocation_examples = self._process_collocation_example(tree=html)
-                        sub_grammar_examples = self._process_grammar_example(tree=html)
+                        sense_refs = self._process_refs(tree=sense)
+                        sense_examples = self._process_example(tree=sense)
+                        sense_collocation_examples = self._process_collocation_example(tree=sense)
+                        sense_grammar_examples = self._process_grammar_example(tree=sense)
 
-                        logger.debug(f"process_sub_senseL {process_sub_sense}")
-                        logger.debug(f"sub_refsL {sub_refs}")
-                        logger.debug(f"sub_examplesL {sub_examples}")
-                        logger.debug(f"sub_collocation_examplesL {sub_collocation_examples}")
-                        logger.debug(f"sub_grammar_examplesL {sub_grammar_examples}")
+                        process_sense.update({'sense_refs': sense_refs}) if sense_refs else None
+                        process_sense.update({'sense_examples': sense_examples}) if sense_examples else None
+                        process_sense.update({'sense_collocation_examples': sense_collocation_examples}) \
+                            if sense_collocation_examples else None
+                        process_sense.update({'sense_grammar_examples': sense_grammar_examples}) if \
+                            sense_grammar_examples else None
 
-                    logger.debug("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+                        process_sense.update({'sub_senses': []})
+
+                        for sub_sense in sense.xpath("span[@class='Subsense']"):
+                            process_sub_sense = self._process_sub_sense(tree=sub_sense)
+
+                            sub_refs = self._process_refs(tree=sub_sense)
+                            sub_examples = self._process_example(tree=sub_sense)
+                            sub_collocation_examples = self._process_collocation_example(tree=sub_sense)
+                            sub_grammar_examples = self._process_grammar_example(tree=sub_sense)
+
+                            process_sub_sense.update({'sub_refs': sub_refs}) if sub_refs else None
+                            process_sub_sense.update({'sub_examples': sub_examples}) if sub_examples else None
+                            process_sub_sense.update({'sub_collocation_examples': sub_collocation_examples}) \
+                                if sub_collocation_examples else None
+                            process_sub_sense.update({'sub_grammar_examples': sub_grammar_examples}) \
+                                if sub_grammar_examples else None
+
+                            process_sense['sub_senses'].append(process_sub_sense)
+
+                        senses.append(process_sense)
+                    headers['senses'] = senses
+                    definitions[headers['homnum']] = headers
+
+        with open('test.json', '+w') as file:
+            file.write(ujson.dumps(definitions))
 
         return definitions
