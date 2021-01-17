ac97ace8a596d353480c495f22a76b656226a7fd
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-01-17 12:42:22 +0330

    update setup

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ INSTALL_REQUIREMENTS = (
     'Scrapy',
 )
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='dictionary_crawler',
     version=dictionary_crawlers.__version__,
@@ -15,6 +18,14 @@ setup(
     license='MIT',
     author='Mohammad Masoumi',
     author_email='mohammad.masoomy74@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='A dictionary crawler written in scrapy in order to crawl longman, combridge and oxford dictionaries.',
     install_requires=INSTALL_REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
